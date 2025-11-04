a482cf0a7c1b3f093d8c67efa24aed41e6c3b26c
domingasp <domingaspleckauskas@capacitas.co.uk>
2025-11-04 19:46:17 +0000

    Style Footer + fix native scroll flicker

---
+++ src/components/layout/stack/stack.variants.ts
@@ -0,0 +1,21 @@
+import { tv, type VariantProps } from "tailwind-variants";
+
+import { spacing } from "../spacing.variants";
+
+export const stack = tv({
+  base: "flex flex-col",
+  defaultVariants: {
+    align: "start",
+    spacing: "md",
+  },
+  extend: spacing,
+  variants: {
+    align: {
+      center: "items-center",
+      end: "items-end",
+      start: "items-start",
+    },
+  },
+});
+
+export type StackVariants = VariantProps<typeof stack>;

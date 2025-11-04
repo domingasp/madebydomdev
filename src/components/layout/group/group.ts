98270c33b1b9dd07731620d31a26ada1280a2145
domingasp <domingaspleckauskas@capacitas.co.uk>
2025-11-04 15:17:44 +0000

    Add ThemeToggle

---
+++ src/components/layout/group/group.ts
@@ -0,0 +1,29 @@
+import { tv, type VariantProps } from "tailwind-variants";
+import { spacing } from "../spacing.variants";
+
+export const group = tv({
+  base: "flex flex-row items-start",
+  defaultVariants: {
+    align: "center",
+    justify: "start",
+    spacing: "md",
+  },
+  extend: spacing,
+  variants: {
+    align: {
+      center: "items-center",
+      end: "items-end",
+      start: "items-start",
+    },
+    justify: {
+      around: "justify-around",
+      between: "justify-between",
+      center: "justify-center",
+      end: "justify-end",
+      evenly: "justify-evenly",
+      start: "justify-start",
+    },
+  },
+});
+
+export type GroupVariants = VariantProps<typeof group>;
